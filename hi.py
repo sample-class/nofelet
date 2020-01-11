59724a218ccb41b3f0958d308eec5b7a4b5f40b5
i <ibelyalov@yandex.ru>
2020-01-11 03:10:15 +0300

    Implement

---
+++ hi.py
@@ -0,0 +1,11 @@
+statistics = {
+"monday": 32600,
+"tuesday": 44800,
+"wednesday": 35200,
+"thursday": 41900,
+"friday": 43700,
+"saturday": 45200,
+"sunday": 50400
+}
+
+print(sum(statistics.values()) / 7)
